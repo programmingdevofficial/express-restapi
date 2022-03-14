13c947bd4b5ad8226e35ec2defdfa593f525427b
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-14 19:16:15 +0530

    controller: users

--- controller/users.js
+++ controller/users.js
@@ -1,14 +1,3 @@
 import { v4 as uuid } from "uuid";
 
 let users = [];
-
-export const getUsers = (req, res) => {
-    console.log(`Users: ${users}`);
-    res.send(users);
-}
-
-export const createUser = (req, res) => {
-    const user = req.body;
-    user.push({...user, id: uuid()});
-    console.log(`User [$]`)
-};
