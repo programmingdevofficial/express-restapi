b6d7e0b815a171299e227f5307d9f70cc4633837
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-15 18:54:46 +0530

    routes: update routes[get, post, delete, patch]

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {createUser, deleteUser, getUsers, updateUser, getUser} from "../controller/users";
 
 const router = express.Router();
+
+router.get('/', getUsers);
+router.post('/', createUser);
+router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
 
 export default router;
